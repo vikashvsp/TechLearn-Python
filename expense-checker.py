9d869374c98917ddb4419d045dd8bdfa78b3169a
vikash kumar <vkvikashkumar987@gmail.com>
2021-06-23 15:07:01 +0530

    expense-check

---
+++ expense-checker.py
@@ -0,0 +1,18 @@
+transaction=int(input("Number of Transaction?"))
+budget=int(input("What's your budget?"))
+ticket=0
+travel=0
+food=0
+while transaction>0:
+    
+    type=int(input("Choose:- 1.Travel 2.Ticket 3.Food"))
+    spent=int(input("How much you spent?"))
+    budget-=spent
+    if type==1:
+        travel+=spent
+    elif type==2:
+        ticket+=spent
+    else: food+= spent
+    if budget<=0:
+        print("Your budget exhausted!")
+        break
