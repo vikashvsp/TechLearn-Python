f567b0056a63e3f6b1a7002d79004b115dc3ccdb
vikash kumar <vkvikashkumar987@gmail.com>
2021-06-23 14:04:07 +0530

    first-problem-done

--- trip-planner.py
+++ trip-planner.py
@@ -1,3 +1,10 @@
+distance_A=int(input("Enter the distance of Casa Manco: "))
+distance_B=int(input("Enter the distance of Colosseum: "))
+distance_C=int(input("Enter the distance of Vatican City: "))
 
-firstname= input("your name: ")
-print("your name is",firstname)
+
+if distance_A<distance_B and distance_A<distance_C:
+    print("Go to Casa Manco")
+elif distance_B<distance_A and distance_B<distance_C:
+    print("Go to Colosseum")
+else : print("Go to Vatican City")
