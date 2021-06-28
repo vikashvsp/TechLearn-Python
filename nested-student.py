095ab1ec921a4835033fcd7db78332a32e44a12e
vikash kumar <vkvikashkumar987@gmail.com>
2021-06-28 23:11:26 +0530

    bug-fixed

--- nested-student.py
+++ nested-student.py
@@ -11,9 +11,10 @@ for i in range(n):
 print(stu_record)
 print("The count of Student is")
 print(len(stu_record))
+
 stu_roll=int(input("Enter roll number whose email ID you want to change:- "))
 new_email=str(input("Enter the new email ID:- "))
-stu_record.update({stu_roll:{'name':stu_name,'email-id':new_email,"roll":stu_roll,'section':stu_section}})
+stu_record[stu_roll] ['email-id']=new_email
 print(stu_record)
 
 pop_roll=int(input("Enter the roll number of student whose record you want to delete:- "))
